91bde8491c119f240fe4e34a53e71d70f194b04a
Sheila Duong <sheiladuong235@gmail.com>
2025-05-05 13:12:37 -0400

    Attempted to add console.log output

--- test/watcher.test.js
+++ test/watcher.test.js
@@ -11,5 +11,7 @@ test('watch correctly outputs that file was parsed', () => {
 
     watcher.watch(watched, output, processed);
 
+    // console.log('\x1b[38;2;0;0;170m%s\x1b[0m', consoleSpy);
+
     expect(consoleSpy).toHaveBeenCalledWith(watched);
 });
