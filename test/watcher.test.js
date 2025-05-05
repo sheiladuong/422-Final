d59af0e563725925a3caafd35d7c8238a7b3b78f
Sheila Duong <sheiladuong235@gmail.com>
2025-05-05 12:20:16 -0400

    Attempted unit test for watch function

---
+++ test/watcher.test.js
@@ -0,0 +1,10 @@
+const { watch } = require('../src/watcher.js');
+
+// correctly outputs that file was parsed
+test('watch correctly outputs that file was parsed', () => {
+    const watched = "/test_inbound";
+    const output = "/test_outbound";
+    const processed = "/test_processed";
+
+    expect(watch(watched, output, processed)).toThrow(console.info('Parsed /Users/sheiladuong/iste422/final-exam/test/test_inbound/parser_test_data.csv'));
+});
